73744360fb11471a786647ea84f5d7e70308e5ed
DilmyPerera <samadiniperera20@gmail.com>
2025-03-15 13:56:22 +0530

    Create `P6.java`

---
+++ P6.java
@@ -0,0 +1,12 @@
+/*
+ 6) Create a student class
+	-create 5 student objects and store it in 
+	a proper datastructure
+- find the average marks of the students
+ */
+
+public class P6 {
+    public static void main(String args[]) {
+
+    }
+}
