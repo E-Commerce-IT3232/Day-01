69c8f7de13e9bd52c1e2a3c319d911b39d73ae1c
DilmyPerera <samadiniperera20@gmail.com>
2025-03-15 14:46:31 +0530

    Constructor to initialize product

--- P8.java
+++ P8.java
@@ -15,4 +15,9 @@ class Product {
     private double price;
     private int quantity;
 
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
