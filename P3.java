74534e6f52acfe67aa1339b1993a4d88abbae69e
DilmyPerera <samadiniperera20@gmail.com>
2025-03-15 13:21:55 +0530

    Implement reverse number printing based on user input

--- P3.java
+++ P3.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 
 public class P3 {
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter the first number : ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter the second number : ");
+        int num2 = sc.nextInt();
+
+        int start = Math.max(num1, num2);
+        int end = Math.min(num1, num2);
+
+        for (int i = start; i >= end; i--) {
+            System.out.println(i);
+        }
+
+        sc.close();
     }
 }
