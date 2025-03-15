a1113ae6ab0eee03afae77b763668cadd83e0d3a
DilmyPerera <samadiniperera20@gmail.com>
2025-03-15 14:45:34 +0530

    Import ArrayList and List for dynamic data storage

---
+++ P8.java
@@ -0,0 +1,11 @@
+/*
+ * implement classes for the given scenario:
+ * - A reatail store which can store,edit,and delete
+ * multible products like fruits, vegetables, grocery, etc..
+ * - display the avilabe products like price, name,
+ * quantity
+ * 
+ */
+
+import java.util.ArrayList;
+import java.util.List;s
