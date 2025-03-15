aa1f88f679075f28a4fb6bd92b42b9757b9f5a81
DilmyPerera <samadiniperera20@gmail.com>
2025-03-15 14:21:00 +0530

    Constructor to initialize Book object

--- P7.java
+++ P7.java
@@ -11,4 +11,8 @@ class Book {
     private String title;
     private String author;
 
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
 }
